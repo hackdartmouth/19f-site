2ce89e275ec9292eb0907720387acde8689d4963
cheniel <danielchen627@gmail.com>
2015-07-31 00:48:47 -0700

    add pine-schedule and placeholder schedules

---
+++ js/main.js
@@ -0,0 +1,185 @@
+PINE_SCHEDULE = {
+  OPTIONALS: {
+    "borders": true,
+    "background_color": "#FFFFFF",
+    "header_alignment": "center"
+  },
+  RANGE: {
+    "start": 9.0,
+    "end": 23.0
+  }
+};
+
+PineSchedule.load(".day-1", {
+  "optionals": PINE_SCHEDULE.OPTIONALS,
+  "day_of_week": "FRIDAY",
+  "month": "OCT",
+  "day": 2,
+  "range": PINE_SCHEDULE.RANGE,
+  "events": [
+    {
+      "time_range": "10-11am",
+      "name": "REGISTRATION",
+      "location": "Cook Auditorium",
+      "color": "#8BB9B6",
+      "start": 10.0,
+      "end": 11.0
+    },
+    {
+      "time_range": "11am-12:30pm",
+      "name": "OPENING SESSION",
+      "location": "Cook Auditorium",
+      "color": "#E6D275",
+      "start": 11.0,
+      "end": 12.5
+    },
+    {
+      "time_range": "1-2pm",
+      "name": "LUNCH",
+      "location": "Thayer Atrium",
+      "color": "#BFDC85",
+      "start": 13.0,
+      "end": 14.0
+    },
+    {
+      "time_range": "2:30-3:30pm",
+      "name": "WORKSHOP",
+      "location": "Couch Lab",
+      "color": "#EBEF8F",
+      "start": 14.5,
+      "end": 15.5
+    },
+    {
+      "time_range": "7-8pm",
+      "name": "DINNER",
+      "location": "Thayer Atrium",
+      "color": "#BFDC85",
+      "start": 19.0,
+      "end": 20.0
+    },
+    {
+      "time_range": "9-9:30pm",
+      "name": "SHEBA SHOW",
+      "location": "Great Hall",
+      "color": "#DF8B80",
+      "start": 21.0,
+      "end": 21.5
+    }
+  ]
+});
+
+PineSchedule.load(".day-2", {
+  "optionals": PINE_SCHEDULE.OPTIONALS,
+  "day_of_week": "SATURDAY",
+  "month": "OCT",
+  "day": 3,
+  "range": PINE_SCHEDULE.RANGE,
+  "events": [
+    {
+      "time_range": "10-11am",
+      "name": "REGISTRATION",
+      "location": "Cook Auditorium",
+      "color": "#8BB9B6",
+      "start": 10.0,
+      "end": 11.0
+    },
+    {
+      "time_range": "11am-12:30pm",
+      "name": "OPENING SESSION",
+      "location": "Cook Auditorium",
+      "color": "#E6D275",
+      "start": 11.0,
+      "end": 12.5
+    },
+    {
+      "time_range": "1-2pm",
+      "name": "LUNCH",
+      "location": "Thayer Atrium",
+      "color": "#BFDC85",
+      "start": 13.0,
+      "end": 14.0
+    },
+    {
+      "time_range": "2:30-3:30pm",
+      "name": "WORKSHOP",
+      "location": "Couch Lab",
+      "color": "#EBEF8F",
+      "start": 14.5,
+      "end": 15.5
+    },
+    {
+      "time_range": "7-8pm",
+      "name": "DINNER",
+      "location": "Thayer Atrium",
+      "color": "#BFDC85",
+      "start": 19.0,
+      "end": 20.0
+    },
+    {
+      "time_range": "9-9:30pm",
+      "name": "SHEBA SHOW",
+      "location": "Great Hall",
+      "color": "#DF8B80",
+      "start": 21.0,
+      "end": 21.5
+    }
+  ]
+});
+
+PineSchedule.load(".day-3", {
+  "optionals": PINE_SCHEDULE.OPTIONALS,
+  "day_of_week": "SUNDAY",
+  "month": "OCT",
+  "day": 4,
+  "range": PINE_SCHEDULE.RANGE,
+  "events": [
+    {
+      "time_range": "10-11am",
+      "name": "REGISTRATION",
+      "location": "Cook Auditorium",
+      "color": "#8BB9B6",
+      "start": 10.0,
+      "end": 11.0
+    },
+    {
+      "time_range": "11am-12:30pm",
+      "name": "OPENING SESSION",
+      "location": "Cook Auditorium",
+      "color": "#E6D275",
+      "start": 11.0,
+      "end": 12.5
+    },
+    {
+      "time_range": "1-2pm",
+      "name": "LUNCH",
+      "location": "Thayer Atrium",
+      "color": "#BFDC85",
+      "start": 13.0,
+      "end": 14.0
+    },
+    {
+      "time_range": "2:30-3:30pm",
+      "name": "WORKSHOP",
+      "location": "Couch Lab",
+      "color": "#EBEF8F",
+      "start": 14.5,
+      "end": 15.5
+    },
+    {
+      "time_range": "7-8pm",
+      "name": "DINNER",
+      "location": "Thayer Atrium",
+      "color": "#BFDC85",
+      "start": 19.0,
+      "end": 20.0
+    },
+    {
+      "time_range": "9-9:30pm",
+      "name": "SHEBA SHOW",
+      "location": "Great Hall",
+      "color": "#DF8B80",
+      "start": 21.0,
+      "end": 21.5
+    }
+  ]
+});
---
+++ js/vendor/pine-schedule.js
@@ -0,0 +1,127 @@
+/* -----------------------------------------------
+/* Author : Daniel Chen - danieljchen.com
+/* MIT license: http://opensource.org/licenses/MIT
+/* GitHub : github.com/cheniel/pine-schedule.js
+/* ----------------------------------------------- */
+window.PineSchedule = {};
+window.PineSchedule.load = function(dom_id, data) {
+  if (!data.optionals) { data.optionals = {}; }
+  var TB_DISPLAY_TYPE = {
+    NONE: 0,
+    ONE_LINE: 1,
+    MULTI_LINE_FIRST: 2,
+    MULTI_LINE_SECOND: 3,
+  };
+
+  var DEFAULT = {
+    TIME_BLOCK_COLOR: data.optionals.background_color,
+    HEADER_ALIGNMENT: data.optionals.header_alignment ? data.optionals.header_alignment : "right",
+    HEADER_FONT_SIZE: data.optionals.header_font_size ? data.optionals.header_font_size : "25px",
+    HEADER_FONT: data.optionals.header_font ? data.optionals.header_font : "Verdana, Geneva, sans-serif",
+    BORDERS: !!data.optionals.borders
+  };
+
+  var container = d3.select(dom_id).style("font-family", DEFAULT.HEADER_FONT);
+  var table = container.append("table");
+
+  var header = table.append("thead");
+  header.append("th");
+  header_data = header.append("th");
+  header_data.style("text-align", DEFAULT.HEADER_ALIGNMENT).style("font-size", DEFAULT.HEADER_FONT_SIZE);
+  header_data.append("b").text(data.day_of_week);
+  header_data.append("span").text(", " + data.month + " " + data.day);
+
+  var data_at_time = [];
+  data.events.forEach(function(e) {
+    time_blocks_for_event = (e.end - e.start) / 0.5;
+    time_block_number = 1;
+    for (var time = e.start; time < e.end; time += 0.5) {
+      if (time in data_at_time) {
+        console.log("PineSchedule: skipping event " + e.name + " because overlap");
+        continue;
+      }
+
+      if (time_blocks_for_event < 1) {
+        console.log("PineSchedule: skipping event " + e.name + " because duration");
+        continue;
+      } 
+
+      time_block_display_type = TB_DISPLAY_TYPE.NONE;
+      if (time_blocks_for_event === 1 && time_block_number === 1) {
+        time_block_display_type = TB_DISPLAY_TYPE.ONE_LINE;
+      } else if (time_blocks_for_event > 1 && time_block_number === 1) {
+        time_block_display_type = TB_DISPLAY_TYPE.MULTI_LINE_FIRST;
+      } else if (time_blocks_for_event > 1 && time_block_number === 2) {
+        time_block_display_type = TB_DISPLAY_TYPE.MULTI_LINE_SECOND;
+      }
+      data_at_time[time] = { event: e, display_type: time_block_display_type };
+      time_block_number++;
+    }
+  });
+
+  var time_blocks = [];
+  for (var t = data.range.start; t < data.range.end; t += 0.5) {
+    d = (data_at_time[t] || {});
+    time_blocks.push(new TimeBlock(t, d.event, d.display_type));
+  }
+
+  table.style("width", "100%").style("border-collapse", "collapse");
+  rows = table.selectAll("tr").data(time_blocks).enter().append("tr").style("height", "25px");
+
+  time_column = rows.append("td")
+    .style("text-align", "right")
+    .style("width", "10%")
+    .style("border-right", "solid #D3D3D3 2px")
+    .style("padding-right", "5px")
+    .text(function(d) { return d.get_pretty_time(); });
+
+  values_column = rows.append("td")
+    .style("background-color", function(d) { return d.color(); })
+    .style("padding-left", "5px")
+    .html(function(d) { return d.get_displayed_info(); });
+
+  if (DEFAULT.BORDERS) {
+    values_column.style("border-right", "solid #D3D3D3 2px");
+    values_column.style("border-top", function(d) { return d.get_border_top(); });
+    d3.select(values_column.node()).style("border-top", "solid #D3D3D3 2px");
+    d3.select(values_column[0][values_column.size() - 1]).style("border-bottom", "solid #D3D3D3 2px");
+  }
+
+  function TimeBlock(time, event, display_type) {
+    this.time = time;
+    this.event = event;
+    this.display_type = display_type;
+    this.is_top_of_hour = function() {
+      return this.time * 10 % 10 === 0;
+    };
+    this.get_border_top = function() {
+      if (this.is_top_of_hour()) {
+        return "dotted #D3D3D3 1px";
+      } else {
+        return "dashed #D3D3D3 1px";
+      }
+    };
+    this.get_pretty_time = function() {
+      if (this.is_top_of_hour()) {
+        if (this.time <= 12) {
+          return this.time + "am";
+        } else {
+          return (this.time - 12) + "pm";
+        }
+      }
+    };
+    this.get_displayed_info = function() {
+      switch(this.display_type) {
+        case TB_DISPLAY_TYPE.ONE_LINE:
+          return this.event.time_range + " | <b>" + this.event.name + "</b> <i>" + this.event.location + "</i>";
+        case TB_DISPLAY_TYPE.MULTI_LINE_FIRST:
+          return this.event.time_range + " | <b>" + this.event.name + "</b>";
+        case TB_DISPLAY_TYPE.MULTI_LINE_SECOND:
+          return "<i>" + this.event.location + "</i>";
+      }
+    };
+    this.color = function() {
+      return this.event ? this.event.color : DEFAULT.TIME_BLOCK_COLOR;
+    };
+  }
+};
