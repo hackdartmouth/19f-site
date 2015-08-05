facfed5a986de5b3671b0e39b39e865a68453d64
cheniel <danielchen627@gmail.com>
2015-08-04 21:35:17 -0700

    change from 3 day to 2 day

--- js/main.js
+++ js/main.js
@@ -12,64 +12,6 @@ PINE_SCHEDULE = {
 
 PineSchedule.load(".day-1", {
   "optionals": PINE_SCHEDULE.OPTIONALS,
-  "day_of_week": "FRI",
-  "month": "OCT",
-  "day": 2,
-  "range": PINE_SCHEDULE.RANGE,
-  "events": [
-    {
-      "time_range": "10-11am",
-      "name": "REGISTRATION",
-      "location": "Cook Auditorium",
-      "color": "#8BB9B6",
-      "start": 10.0,
-      "end": 11.0
-    },
-    {
-      "time_range": "11am-12:30pm",
-      "name": "OPENING SESSION",
-      "location": "Cook Auditorium",
-      "color": "#E6D275",
-      "start": 11.0,
-      "end": 12.5
-    },
-    {
-      "time_range": "1-2pm",
-      "name": "LUNCH",
-      "location": "Thayer Atrium",
-      "color": "#BFDC85",
-      "start": 13.0,
-      "end": 14.0
-    },
-    {
-      "time_range": "2:30-3:30pm",
-      "name": "WORKSHOP",
-      "location": "Couch Lab",
-      "color": "#EBEF8F",
-      "start": 14.5,
-      "end": 15.5
-    },
-    {
-      "time_range": "7-8pm",
-      "name": "DINNER",
-      "location": "Thayer Atrium",
-      "color": "#BFDC85",
-      "start": 19.0,
-      "end": 20.0
-    },
-    {
-      "time_range": "9-9:30pm",
-      "name": "SHEBA SHOW",
-      "location": "Great Hall",
-      "color": "#DF8B80",
-      "start": 21.0,
-      "end": 21.5
-    }
-  ]
-});
-
-PineSchedule.load(".day-2", {
-  "optionals": PINE_SCHEDULE.OPTIONALS,
   "day_of_week": "SAT",
   "month": "OCT",
   "day": 3,
@@ -126,7 +68,7 @@ PineSchedule.load(".day-2", {
   ]
 });
 
-PineSchedule.load(".day-3", {
+PineSchedule.load(".day-2", {
   "optionals": PINE_SCHEDULE.OPTIONALS,
   "day_of_week": "SUN",
   "month": "OCT",
