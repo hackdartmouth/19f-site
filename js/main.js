bf1d8d6be7db33e61037fbb2d1d302e8d085ae40
emmaoberstein <emmaoberstein@gmail.com>
2016-04-05 19:37:56 -0400

    updated hackday info and schedule

--- js/main.js
+++ js/main.js
@@ -4,174 +4,112 @@ PINE_SCHEDULE = {
     "background_color": "#FFFFFF",
     "header_alignment": "center"
   },
-  DAY_1_START: 10.0,
-  DAY_2_START: 7.0,
-  HOURS_TO_SHOW: 15,
+  DAY_1_START: 8.0,
+  DAY_2_START: 8.0,
+  HOURS_TO_SHOW: 13,
 };
-
-PineSchedule.load(".day-0", {
-  "optionals": PINE_SCHEDULE.OPTIONALS,
-  "day_of_week": "FRI",
-  "month": "OCT",
-  "day": 2,
-  "range": {
-    "start": 16.0,
-    "end": 17.0
-  },
-  "events": [
-    {
-      "time_range": "4-5pm",
-      "name": "Microsoft Workshop",
-      "location": "Maclean B01",
-      "color": "#E6F176",
-      "start": 16.0,
-      "end": 17.0
-    },
-  ]
-});
 
 
 PineSchedule.load(".day-1", {
   "optionals": PINE_SCHEDULE.OPTIONALS,
-  "day_of_week": "SAT",
-  "month": "OCT",
-  "day": 3,
+  "day_of_week": "FRIDAY",
+  "month": "APRIL",
+  "day": 15,
   "range": {
     "start": PINE_SCHEDULE.DAY_1_START,
     "end": PINE_SCHEDULE.DAY_1_START + PINE_SCHEDULE.HOURS_TO_SHOW
   },
   "events": [
     {
-      "time_range": "10-11am",
-      "name": "Registration",
-      "location": "Filene Auditorium - Moore",
-      "color": "#8BB9B6",
-      "start": 10.0,
-      "end": 11.0
-    },
-    {
-      "time_range": "11am-12pm",
-      "name": "Opening Ceremony",
-      "location": "Filene Auditorium - Moore",
-      "color": "#E0CC5C",
-      "start": 11.0,
-      "end": 12.5
-    },
-    {
-      "time_range": "12:30-1:30pm",
-      "name": "Lunch (Moe's), Team Formation, Hacking!",
-      "location": "Haldeman",
-      "color": "#B2D96D",
-      "start": 12.5,
-      "end": 13.5
-    },
-    {
-      "time_range": "1:30-2:30pm",
-      "name": "DEN Talk: Beyond the Hack",
-      "location": "Kemeny 008",
+      "time_range": "5-6pm",
+      "name": "Tech Panel",
+      "location": "Location TBD",
       "color": "#E6F176",
-      "start": 13.5,
-      "end": 14.5
+      "start": 17.0,
+      "end": 18.0
     },
     {
-      "time_range": "2:30-3:30pm",
-      "name": "Intralinks Workshop",
-      "location": "Kemeny 008",
+      "time_range": "6-7pm",
+      "name": "HTML/CSS Workshop",
+      "location": "Location TBD",
       "color": "#E6F176",
-      "start": 14.5,
-      "end": 15.5
+      "start": 18.0,
+      "end": 19.0
     },
     {
-      "time_range": "3:30-4:30pm",
-      "name": "Thayer Workshop: Hacking the Raspberry Pi",
-      "location": "Kemeny 008",
+      "time_range": "7-8pm",
+      "name": "Building Bots Workshop",
+      "location": "Location TBD",
       "color": "#E6F176",
-      "start": 15.5,
-      "end": 16.5
-    },
-    {
-      "time_range": "4:30-5pm",
-      "name": "Presenting Your Experience on Paper",
-      "location": "Kemeny 008",
-      "color": "#E6F176",
-      "start": 16.5,
-      "end": 17.0
-    },
-    {
-      "time_range": "7pm",
-      "name": "Dinner (Gas Station Chinese)",
-      "location": "Haldeman",
-      "color": "#B2D96D",
       "start": 19.0,
       "end": 20.0
-    },
-    {
-      "time_range" : "12-1am",
-      "name": "Midnight Activity!",
-      "location": "Haldeman",
-      "color": "#D7766B",
-      "start": 24.0,
-      "end": 25.0
-    },
+    }
   ]
 });
 
 PineSchedule.load(".day-2", {
   "optionals": PINE_SCHEDULE.OPTIONALS,
-  "day_of_week": "SUN",
-  "month": "OCT",
-  "day": 4,
+  "day_of_week": "SATURDAY",
+  "month": "APRIL",
+  "day": 16,
   "range": {
     "start": PINE_SCHEDULE.DAY_2_START,
     "end": PINE_SCHEDULE.DAY_2_START + PINE_SCHEDULE.HOURS_TO_SHOW
   },
   "events": [
-    {
-      "time_range": "8-9:30am",
-      "name": "Breakfast (Lou's)",
-      "location": "Haldeman",
-      "color": "#B2D96D",
+  {
+      "time_range": "8am",
+      "name": "Registration Begins and Continues Throughout the Day",
+      "location": "DEN - 4 Currier Place",
+      "color": "#8BB9B6",
       "start": 8.0,
-      "end": 9.5
+      "end":9.0
     },
     {
-      "time_range": "10am",
-      "name": "Project Submissions Due",
-      "color": "#8BB9B6",
+      "time_range": "9am",
+      "name": "Breakfast (Lou's)",
+      "location": "DEN",
+      "color": "#B2D96D",
+      "start": 9.0,
+      "end": 10.0
+    },
+    {
+      "name": "Hacking!",
+      "location": "DEN",
+      "color": "#D7766B",
       "start": 10.0,
-      "end": 10.5,
-    },
-    {
-      "time_range": "10:30am-12pm",
-      "name": "Project demos and First Round Judging",
-      "location": "Kemeny 006 and 007",
-      "color": "#8BB9B6",
-      "start": 10.5,
-      "end": 12.0,
+      "end": 12.0
     },
     {
       "time_range": "12-1pm",
-      "name": "Lunch (Subway)",
-      "location": "Haldeman",
+      "name": "Lunch (Gas Station Chinese)",
+      "location": "DEN",
       "color": "#B2D96D",
       "start": 12.0,
       "end": 13.0
     },
     {
-      "time_range": "1-3pm",
-      "name": "Closing Ceremony",
-      "location": "Filene Auditorium - Moore",
-      "color": "#E0CC5C",
+      "name": "Hacking!",
+      "location": "DEN",
+      "color": "#D7766B",
       "start": 13.0,
-      "end": 15.0
+      "end": 19.0
     },
     {
-      "time_range": "3:30pm",
-      "name": "Buses to NY and Boston depart",
-      "location": "The Hopkins Center for the Arts",
-      "color": "#BFBFBF",
-      "start": 15.5,
-      "end": 16.5
+      "time_range": "7pm",
+      "name": "Dinner (Boloco)",
+      "location": "DEN",
+      "color": "#B2D96D",
+      "start": 19.0,
+      "end": 20.0
+    },
+    {
+      "time_range": "8pm",
+      "name": "Judging and Prizes",
+      "location": "DEN",
+      "color": "#E0CC5C",
+      "start": 20.0,
+      "end": 21.0
     }
   ]
 });
@@ -228,9 +166,6 @@ function jumpToSection(event, targetSection){
 }
 
 $(document).ready(function(){
-
-  // hide the schedule for now
-  $('#schedule').hide();
 
   var faqHeight = $('#faq').height();
   var faqWidth = $('#faq').width();
