32d2647d5946dc4881b2a69effc18273018491d8
emmaoberstein <emmaoberstein@gmail.com>
2016-07-17 07:03:32 -0400

    applications up

--- js/main.js
+++ js/main.js
@@ -175,7 +175,7 @@ function jumpToSection(event, targetSection){
 $(document).ready(function(){
 
   $('#schedule').hide();
-  $('#apply').hide();
+  //$('#apply').hide();
 
   var faqHeight = $('#faq').height();
   var faqWidth = $('#faq').width();
