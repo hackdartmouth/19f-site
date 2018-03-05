3b065cb026c38a94e8e62b91e5c4bce7644d6611
Henry Foster <henrythefoster@gmail.com>
2018-03-05 00:41:42 -0500

    Added schedule and tracks section. Tracks still has to be styled

--- js/main.js
+++ js/main.js
@@ -4,116 +4,163 @@ PINE_SCHEDULE = {
     "background_color": "#FFFFFF",
     "header_alignment": "center"
   },
-  DAY_1_START: 9.0,
-  DAY_2_START: 9.0,
-  HOURS_TO_SHOW: 13,
+  DAY_1_START: 5.5,
+  DAY_2_START: 7,
+  HOURS_TO_SHOW: 19.5,
 };
 
 
 PineSchedule.load(".day-1", {
   "optionals": PINE_SCHEDULE.OPTIONALS,
-  "day_of_week": "FRIDAY",
+  "day_of_week": "SATURDAY",
   "month": "APRIL",
-  "day": 7,
+  "day": 14,
   "range": {
     "start": PINE_SCHEDULE.DAY_1_START,
     "end": PINE_SCHEDULE.DAY_1_START + PINE_SCHEDULE.HOURS_TO_SHOW
   },
   "events": [
     {
-      "time_range": "5:30pm-6:30pm",
-      "name": "Google Web-Dev Workshop",
-      "location": "Collis 101",
-      "color": "#8BB9B6",
-      "start": 17.5,
-      "end": 18.5
+      "time_range": "5:30am",
+      "name": "New York bus departs",
+      "location": "TBD",
+      "color": "#CADD74",
+      "start": 5.5,
+      "end": 6
     },
     {
-      "time_range": "6:30pm-7:00pm",
-      "name": "Dinner from Tuk-Tuk Thai",
-      "location": "Collis 101",
-      "color": "#E6F176",
-      "start": 18.5,
-      "end": 19.0
+      "time_range": "8am",
+      "name": "Boston bus departs",
+      "location": "TBD",
+      "color": "#689540",
+      "start": 8,
+      "end": 8.5
     },
     {
-      "time_range": "7:00pm-8:00pm",
-      "name": "DALI Virtual Reality Workshop",
-      "location": "Collis 101",
-      "color": "#B2D96D",
-      "start": 19.0,
-      "end": 20
+      "time_range": "9am-11am",
+      "name": "Check-in",
+      "location": "Sponsors: Glycofi Atrium; Participants: TBD",
+      "color": "#C7DF73",
+      "start": 9,
+      "end": 11
+    },
+    {
+      "time_range": "11am-11:30am",
+      "name": "Opening Ceremony",
+      "location": "TBD",
+      "color": "#74A39B",
+      "start": 11,
+      "end": 11.5
+    },
+    {
+      "time_range": "11:30am-12pm",
+      "name": "Team Formation",
+      "location": "TBD",
+      "color": "#CADD74",
+      "start": 11.5,
+      "end": 12
+    },
+    {
+      "time_range": "12pm",
+      "name": "Hacking Begins; Lunch",
+      "location": "Glycofi Atrium",
+      "color": "#689540",
+      "start": 12,
+      "end": 13
+    },
+    {
+      "time_range": "1pm-6pm",
+      "name": "Workshops",
+      "location": "Couch Lab; Spanos Auditorium",
+      "color": "#74A39B",
+      "start": 13,
+      "end": 18
+    },
+    {
+      "time_range": "6pm",
+      "name": "Dinner",
+      "location": "Glycofi Atrium",
+      "color": "#C7DF73",
+      "start": 18,
+      "end": 19
+    },
+    {
+      "time_range": "11:30pm-12am",
+      "name": "D-style Performance; Raffle",
+      "location": "Cummings Great Hall",
+      "color": "#689540",
+      "start": 23.5,
+      "end": 24
+    },
+    {
+      "time_range": "12am-1am",
+      "name": "Midnight Actvity",
+      "location": "Glycofi Atrium",
+      "color": "#CADD74",
+      "start": 24,
+      "end": 25
     },
   ]
 });
 
 PineSchedule.load(".day-2", {
   "optionals": PINE_SCHEDULE.OPTIONALS,
-  "day_of_week": "SATURDAY",
+  "day_of_week": "SUNDAY",
   "month": "APRIL",
-  "day": 8,
+  "day": 15,
   "range": {
     "start": PINE_SCHEDULE.DAY_2_START,
     "end": PINE_SCHEDULE.DAY_2_START + PINE_SCHEDULE.HOURS_TO_SHOW
   },
   "events": [
-  {
-      "time_range": "9am",
-      "name": "Registration and Breakfast",
-      "location": "Sarner Underground",
-      "color": "#E6F176",
-      "start": 9.0,
-      "end":10.5
+    {
+      "time_range": "7am",
+      "name": "Breakfast",
+      "location": "Glycofi",
+      "color": "#689540",
+      "start": 7,
+      "end": 8
     },
     {
-      "time_range": "",
-      "name": "Hacking",
-      "location": "Sarner Underground",
-      "color": "#D7766B",
-      "start": 10.5,
+      "time_range": "11am",
+      "name": "Lunch",
+      "location": "Glycofi Atrium",
+      "color": "#C7DF73",
+      "start": 11,
+      "end": 12
+    },
+    {
+      "time_range": "12pm",
+      "name": "Submissions due",
+      "location": "TBD",
+      "color": "#74A39B",
+      "start": 12,
       "end": 12.5
     },
     {
-      "time_range": "12pm-1:30pm",
-      "name": "Lunch",
-      "location": "Sarner Underground",
-      "color": "#B2D96D",
-      "start": 12.0,
-      "end": 13.5
+      "time_range": "12:30pm-2pm",
+      "name": "Demos & First-Round Judging",
+      "location": "Glycofi Atrium",
+      "color": "#CADD74",
+      "start": 12.5,
+      "end": 14
     },
     {
-      "time_range": "",
-      "name": "Hacking",
-      "location": "Sarner Underground",
-      "color": "#D7766B",
-      "start": 13.5,
-      "end": 18.0
+      "time_range": "2pm-3pm",
+      "name": "Closing Ceremony",
+      "location": "TBD",
+      "color": "#689540",
+      "start": 14,
+      "end": 15
     },
     {
-      "time_range": "6pm-7:30pm",
-      "name": "Dinner",
-      "location": "Sarner Underground",
-      "color": "#E6F176",
-      "start": 18.0,
-      "end": 19.5
+      "time_range": "4pm",
+      "name": "Buses depart from Hanover",
+      "location": "Hopkin's Center for the Arts",
+      "color": "#74A39B",
+      "start": 16,
+      "end": 16.5
     },
-    {
-      "time_range": "",
-      "name": "Hacking",
-      "location": "Sarner Underground",
-      "color": "#D7766B",
-      "start": 19.5,
-      "end": 21.0
-    },
-    {
-
-      "time_range": "9:00pm-10:00pm",
-      "name": "Judging and Awards",
-      "location": "Sarner Underground",
-      "color": "#8BB9B6",
-      "start": 21.0,
-      "end": 22.0
-    }
   ]
 });
 
@@ -152,6 +199,9 @@ function jumpToSection(event, targetSection){
       break;
     case "s3":
       controlString = "#faq";
+      break;
+    case "s3.5":
+      controlString = "#tracks";
       break;
     case "s4":
       controlString = "#sponsors";
